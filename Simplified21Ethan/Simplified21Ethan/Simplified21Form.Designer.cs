197029081d7fc1827e37b96c3508df409406794e
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-10-29 11:59:40 -0400

    Added Simplified21

---
+++ Simplified21Ethan/Simplified21Ethan/Simplified21Form.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Simplified21Ethan
+{
+    partial class frmSimplified21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmSimplified21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmSimplified21";
+            this.Text = "Simplified 21 by Ethan";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Simplified21Ethan/Simplified21Ethan/Simplified21Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simplified21Ethan
+{
+    public partial class frmSimplified21 : Form
+    {
+        public frmSimplified21()
+        {
+            InitializeComponent();
+        }
+    }
+}
