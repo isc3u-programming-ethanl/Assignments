8133b74e397fa7c6f1926c9fbbaed581deabf177
Ethan LeGault <47784129+s256202@users.noreply.github.com>
2019-10-29 13:09:07 -0400

    Updated Simplified21

--- Simplified21Ethan/Simplified21Ethan/Simplified21Form.cs
+++ Simplified21Ethan/Simplified21Ethan/Simplified21Form.cs
@@ -16,5 +16,34 @@ namespace Simplified21Ethan
         {
             InitializeComponent();
         }
+
+        private void FrmSimplified21_Load(object sender, EventArgs e)
+        {
+            this.btnHit.Visible = false;
+            this.btnStay.Visible = false;
+        }
+
+        private void LblTitleText_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            this.btnHit.Visible = true;
+            this.btnStay.Visible = true;
+            this.lblTitleText.Visible = false;
+            this.btnStart.Visible = false;
+        }
+
+        private void BtnHit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnStay_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
